67991123d3f0c3e3ffddf200b22283ff1c36c7c7
Arnaldo Pérez <arnaldoeperezb@gmail.com>
2025-02-02 14:31:54 -0400

    solucion

--- app.js
+++ app.js
@@ -7,10 +7,45 @@ app.use(Express.json())
 
 // Tu código va desde aquí ⬇️
 
-app.get("/",(req,res)=>{
-  return res.status(200).send("OK")
+
+const tasks = [];
+let id=0
+app.get("/tasks",(req,res)=>{
+  return res.status(200).send(tasks)
 })
 
+app.post("/tasks",(req,res)=>{
+  const body=req.body
+  let newTask=body
+  id++
+  newTask.id=id
+  tasks.push(newTask)
+  return res.status(201).send(newTask)
+})
+
+app.get("/tasks/:id",(req,res)=>{
+  const id=req.params.id
+  let foundTask=tasks.find(task=>task.id==id)
+  return res.status(200).send(foundTask)
+})
+
+
+app.put("/tasks/:id",(req,res)=>{
+  const id=req.params.id
+  const body=req.body
+  let updatedTask=body
+  let foundTask=tasks.find(task=>task.id==id)
+  foundTask.title=updatedTask.title
+  foundTask.description=updatedTask.description
+  foundTask.completed=updatedTask.completed
+  return res.status(200).send(foundTask)
+})
+
+app.delete("/tasks/:id",(req,res)=>{
+  let taksIndex=tasks.findIndex(task=>task.id==req.params.id)
+  tasks.splice(taksIndex,1)
+  return res.status(200).send(tasks)
+})
 // Hasta aquí ⬇⬆️
 
 module.exports={app}
